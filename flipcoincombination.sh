293a19f9796af04a4c8b0558c715bd2887b3f01c
Aditya Gour <adityagour442@gmail.com>
2021-07-26 16:33:15 +0530

    Multiple time flipping the coin store the Doublet combination in a dictionary

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -19,6 +19,30 @@ do
         fi
 done
 
+	for ((i=1; i<=50; i++))
+do
+        randomCheck1=$((RANDOM % 2))
+        randomCheck2=$((RANDOM % 2))
+
+        if (( $randomCheck1 == 0 && $randomCheck2 == 0 ))
+        then
+                echo "H H"
+                ((Singlet[HH]++))
+        elif (( $randomCheck1 == 0 && $randomCheck2 == 1 ))
+        then
+                echo "H T"
+                ((Singlet[HT]++))
+        elif (( $randomCheck1 == 1 && $randomCheck2 == 0 ))
+        then
+                echo "T H"
+                ((Singlet[TH]++))
+
+        else
+                echo "T T"
+                ((Singlet[TT]++))
+        fi
+done
+
 echo ${!Singlet[@]}
 echo ${Singlet[@]}
 
@@ -26,3 +50,11 @@ H=`awk "BEGIN {print (${Singlet[HH]}/50)*100}"`
 echo "H = $H%"
 T=`awk "BEGIN {print (${Singlet[T]}/50)*100}"`
 echo "T = $T%"
+HH=`awk "BEGIN {print (${Singlet[HH]}/50)*100}"`
+echo "HH = $HH%"
+HT=`awk "BEGIN {print (${Singlet[HT]}/50)*100}"`
+echo "HT = $HT%"
+TH=`awk "BEGIN {print (${Singlet[TH]}/50)*100}"`
+echo "TH = $TH%"
+TT=`awk "BEGIN {print (${Singlet[TT]}/50)*100}"`
+echo "TT = $TT%"
