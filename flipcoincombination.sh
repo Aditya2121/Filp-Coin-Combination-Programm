c803a4fbc191344ec6b324e04a1502dcce0431ae
Aditya Gour <adityagour442@gmail.com>
2021-07-26 16:29:05 +0530

    Multiple time flipping the coin store the singlet combination in a dictionary

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -3,5 +3,26 @@
 echo "Welcome to Flip Coin Combination Program"
 
 head=0
-tail=1
-randomCheck1=$((RANDOM % 2))
+declare -A Singlet
+
+for ((i=1; i<=50; i++))
+do
+        randomCheck1=$((RANDOM % 2))
+
+        if (( $randomCheck1 == 0 ))
+        then
+                echo "H"
+                ((Singlet[H]++))
+        else
+                echo "T"
+                ((Singlet[T]++))
+        fi
+done
+
+echo ${!Singlet[@]}
+echo ${Singlet[@]}
+
+H=`awk "BEGIN {print (${Singlet[HH]}/50)*100}"`
+echo "H = $H%"
+T=`awk "BEGIN {print (${Singlet[T]}/50)*100}"`
+echo "T = $T%"
